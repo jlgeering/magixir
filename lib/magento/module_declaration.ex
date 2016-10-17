11d3c420792e3e542f8e026b9d3f79b9a401625b
Jean-Luc Geering <jlgeering.13@gmail.com>
2016-10-17 22:17:24 +0200

    fix credo warning

--- lib/magento/module_declaration.ex
+++ lib/magento/module_declaration.ex
@@ -5,7 +5,10 @@ defmodule Magento.ModuleDeclaration do
   import SweetXml
 
   def load(file) do
-    File.read!(file) |> xpath(~x"//config/modules/*"l) |> Enum.map(&parse_declaration/1)
+    file
+    |> File.read!()
+    |> xpath(~x"//config/modules/*"l)
+    |> Enum.map(&parse_declaration/1)
   end
 
   def parse_declaration(e) do
