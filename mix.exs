f762a311607958922ff433a848eefe1d4b68d689
Unknown <jeanluc.geering@ufirstgroup.com>
2018-08-06 23:29:21 +0200

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Magixir.Mixfile do
   def project do
     [
       app: :magixir,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.2",
       description: description(),
       package: package(),
